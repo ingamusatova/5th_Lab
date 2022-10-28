4c350c088dbabe497e1c6785881400ce4fcea4b6
Emil Gafarov <113677882+gafarovExe@users.noreply.github.com>
2022-10-28 10:23:47 +0300

    Update Level2Task23.cs

--- Level2Task23.cs
+++ Level2Task23.cs
@@ -4,7 +4,7 @@ using System.Windows.Markup;
 
 class HelloWorld
 {
-    static List<int> maxim(List<List<int>> matrix, List<int>maxi)
+    static List<int> maxim(List<List<int>> matrix, List<int> maxi)
     {
         int max = matrix[0][0];
         for (int i = 0; i < matrix.Count; i++)
@@ -36,7 +36,9 @@ class HelloWorld
 
             for (int j = 0; j < m; j++)
             {
-                matrix[i].Add(Convert.ToInt32(line[j]));
+                int x;
+                int.TryParse(line[j], out x);
+                matrix[i].Add(x);
             }
         }
 
@@ -54,9 +56,9 @@ class HelloWorld
     }
     static List<List<int>> preobr1(List<List<int>> matrix, List<int> maxi)
     {
-        for (int i=0; i<matrix.Count; i++)
+        for (int i = 0; i < matrix.Count; i++)
         {
-            for(int j=0; j<matrix[0].Count(); j++)
+            for (int j = 0; j < matrix[0].Count(); j++)
             {
                 if (maxi.Contains(matrix[i][j]) == true) matrix[i][j] = matrix[i][j] * 2;
                 else matrix[i][j] = matrix[i][j] / 2;
@@ -69,15 +71,23 @@ class HelloWorld
         List<List<int>> matrix1 = new List<List<int>>();
         List<List<int>> matrix2 = new List<List<int>>();
         Console.WriteLine("Enter the first size of first matrix: ");
-        int n1 = Convert.ToInt32(Console.ReadLine());
+        int n1;
+        int.TryParse(Console.ReadLine(), out n1);
+        if (n1 <= 0) {Console.WriteLine("Size of matrix can't be less or equal to zero!"); return 0; }
         Console.WriteLine("Enter the second size of first matrix: ");
-        int m1 = Convert.ToInt32(Console.ReadLine());
+        int m1;
+        int.TryParse(Console.ReadLine(), out m1);
+        if (m1 <= 0) { Console.WriteLine("Size of matrix can't be less or equal to zero!"); return 0; }
         Console.WriteLine("Enter first matrix: ");
         vvodmatrici(n1, m1, out matrix1);
         Console.WriteLine("Enter the first size of second matrix: ");
-        int n2 = Convert.ToInt32(Console.ReadLine());
+        int n2;
+        int.TryParse(Console.ReadLine(), out n2);
+        if (n2 <= 0) { Console.WriteLine("Size of matrix can't be less or equal to zero!"); return 0; }
         Console.WriteLine("Enter the second size of second matrix: ");
-        int m2 = Convert.ToInt32(Console.ReadLine());
+        int m2;
+        int.TryParse(Console.ReadLine(), out m2);
+        if (m2 <= 0) { Console.WriteLine("Size of matrix can't be less or equal to zero!"); return 0; }
         Console.WriteLine("Enter second matrix: ");
         vvodmatrici(n2, m2, out matrix2);
 
@@ -85,7 +95,7 @@ class HelloWorld
         List<int> maxi2 = new List<int>();
 
 
-        for (int i=0; i<5; i++)
+        for (int i = 0; i < 5; i++)
         {
             maxim(matrix1, maxi1);
             maxim(matrix2, maxi2);
